266eed22a8840d414490e7f18e428e79f125b8f0
Susi6322 <127908512+Susi6322@users.noreply.github.com>
2023-07-12 11:14:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+
+function bulb_on(){
+    img.src = "./Images/bulb onn.jpg";
+}
+
+function bulb_off(){
+    img.src ="./Images/bulb off.jpg";
+}
